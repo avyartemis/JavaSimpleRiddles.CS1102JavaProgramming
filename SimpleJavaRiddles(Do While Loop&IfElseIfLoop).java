6fbfbe9c8c016ea2ca60b825aeb16c9abd28b1f8
Avy Artemis <87941581+avyartemis@users.noreply.github.com>
2021-09-16 06:01:28 +0800

    Update and rename SimpleJavaRiddles(Do-While Loop&ElseIfLoop).java to SimpleJavaRiddles(Do While Loop&IfElseIfLoop).java

--- SimpleJavaRiddles(Do While Loop&IfElseIfLoop).java
+++ SimpleJavaRiddles(Do While Loop&IfElseIfLoop).java
@@ -5,11 +5,13 @@ public class Quiz {
         String question = "Quiz Time You-All-People!\nWhy did Java cross the road?\n";
         //Create the answer options
         question += "A. To get to the other side.\n";
-        question += "B. To get Coffee!";
+        question += "B. To get Ms Avy Javanese Coffee!\n";
+        question += "C. To get everyone Coffee!\n";
+        question += "D. To get to the Starbucks & try the 'New Seasonal Flavour!'\n";
+        question += "E. To get Nowhere! xD";
 
         //Do While Loop
         do {
-
         String answer = JOptionPane.showInputDialog(question);
 
         answer = answer.toUpperCase();
@@ -18,20 +20,18 @@ public class Quiz {
             JOptionPane.showMessageDialog(null, "Correct!");
             break;
         }
-        else if (answer.equals("A"))
+        else if (answer.equals("A") | answer.equals("C") | answer.equals("D") | answer.equals("E"))
         {
             JOptionPane.showMessageDialog(null, "Incorrect. Please try again.");
             continue;
         }
         else
         {
-            JOptionPane.showMessageDialog(null, "Invalid answer. Please enter A or B");
+            JOptionPane.showMessageDialog(null, "Invalid answer. Please enter A, B, C, D or E");
             continue;
         }
-
     }
     while (true);
-
     }
 }
 
